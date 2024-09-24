f3deee7cc9423b877540e10905f41d320ecfa706
shamiliqbaljj <shamil.iqbal@jobinandjismi.com>
2024-09-24 09:55:18 +0530

    Initial Commit OTP 7886

---
+++ Training/src/FileCabinet/SuiteScripts/JobinAndJismi/OTP/jj_cs_restrict_fulfillment_otp_7886.js
@@ -0,0 +1,208 @@
+/**
+ * 
+ * 
+ * OTP 7886 - Restrict Item Fulfillment
+ * 
+ * 
+ * ------------------------------------------------------------------------
+ * 
+ * Author : Jobin And Jismi IT Services
+ * 
+ * Date Created : 24 - September - 2024
+ * 
+ * Description : This script is for restricting item fulfillment if custom checkbox is checked.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @NApiVersion 2.x
+ * @NScriptType ClientScript
+ * @NModuleScope SameAccount
+ */
+define(['N/record','N/ui/message'],
+
+function(record,message) {
+    
+    /**
+     * Function to be executed after page is initialized.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.mode - The mode in which the record is being accessed (create, copy, or edit)
+     *
+     * @since 2015.2
+     */
+    function pageInit(scriptContext) {
+
+        
+        
+
+    }
+
+    /**
+     * Function to be executed when field is changed.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     * @param {string} scriptContext.fieldId - Field name
+     * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
+     * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
+     *
+     * @since 2015.2
+     */
+    function fieldChanged(scriptContext) {
+
+    }
+
+    /**
+     * Function to be executed when field is slaved.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     * @param {string} scriptContext.fieldId - Field name
+     *
+     * @since 2015.2
+     */
+    function postSourcing(scriptContext) {
+
+    }
+
+    /**
+     * Function to be executed after sublist is inserted, removed, or edited.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @since 2015.2
+     */
+    function sublistChanged(scriptContext) {
+
+    }
+
+    /**
+     * Function to be executed after line is selected.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @since 2015.2
+     */
+    function lineInit(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when field is changed.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     * @param {string} scriptContext.fieldId - Field name
+     * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
+     * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
+     *
+     * @returns {boolean} Return true if field is valid
+     *
+     * @since 2015.2
+     */
+    function validateField(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when sublist line is committed.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     *
+     * @since 2015.2
+     */
+    function validateLine(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when sublist line is inserted.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     *
+     * @since 2015.2
+     */
+    function validateInsert(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when record is deleted.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     *
+     * @since 2015.2
+     */
+    function validateDelete(scriptContext) {
+
+    }
+
+    /**
+     * Validation function to be executed when record is saved.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @returns {boolean} Return true if record is valid
+     *
+     * @since 2015.2
+     */
+    function saveRecord(scriptContext) {
+
+        let currentRecord = scriptContext.currentRecord;
+        let holdFulfillment = currentRecord.getValue('custbody_jj_hold_fulfillment_otp_7886');// Fetch value of custom checkbox
+        if (holdFulfillment)
+        {
+           warningMessage();
+        }
+        return true;
+    }
+
+    // Function To Display Warning Message
+
+    function warningMessage()
+    {
+        let myMsg3 = message.create({
+            title: 'Fulfillment Not Allowed',
+            message: 'Fulfilmment is restricted for this Sales Order',
+            type: message.Type.WARNING,
+            duration: 100000
+        });
+        myMsg3.show();
+    }
+
+    return {
+        // pageInit: pageInit,
+        // fieldChanged: fieldChanged,
+        // postSourcing: postSourcing,
+        // sublistChanged: sublistChanged,
+        // lineInit: lineInit,
+        // validateField: validateField,
+        // validateLine: validateLine,
+        // validateInsert: validateInsert,
+        // validateDelete: validateDelete,
+        saveRecord: saveRecord
+    };
+    
+});
---
+++ Training/src/FileCabinet/SuiteScripts/JobinAndJismi/OTP/jj_ue_restrict_fulfillment_otp_7886.js
@@ -0,0 +1,94 @@
+/**
+ * 
+ * 
+ * 
+ * 
+ *  OTP 7886 - Restrict Item Fulfillment
+ * 
+ * 
+ * ------------------------------------------------------------------------
+ * 
+ * Author : Jobin And Jismi IT Services
+ * 
+ * Date Created : 24 - September - 2024
+ * 
+ * Description : This script is for displaying warning message for item fulfillment if custom checkbox is checked.
+ * 
+ * 
+ * 
+ * 
+ * @NApiVersion 2.1
+ * @NScriptType UserEventScript
+ */
+define(['N/record', 'N/search', 'N/ui/message','N/log', 'N/error'],
+    /**
+ * @param{record} record
+ * @param{search} search
+ * @param{message} message
+ */
+    (record, search, message, log, error) => {
+        /**
+         * Defines the function definition that is executed before record is loaded.
+         * @param {Object} scriptContext
+         * @param {Record} scriptContext.newRecord - New record
+         * @param {string} scriptContext.type - Trigger type; use values from the context.UserEventType enum
+         * @param {Form} scriptContext.form - Current form
+         * @param {ServletRequest} scriptContext.request - HTTP request information sent from the browser for a client action only.
+         * @since 2015.2
+         */
+        const beforeLoad = (scriptContext) => {
+
+        }
+
+        /**
+         * Defines the function definition that is executed before record is submitted.
+         * @param {Object} scriptContext
+         * @param {Record} scriptContext.newRecord - New record
+         * @param {Record} scriptContext.oldRecord - Old record
+         * @param {string} scriptContext.type - Trigger type; use values from the context.UserEventType enum
+         * @since 2015.2
+         */
+        const beforeSubmit = (scriptContext) => {
+
+        var newRecord = scriptContext.newRecord;
+
+        
+        if (newRecord.type === record.Type.ITEM_FULFILLMENT) {
+            var salesOrderId = newRecord.getValue({
+                fieldId: 'createdfrom'
+            });
+            
+                var salesOrder = record.load({
+                    type: record.Type.SALES_ORDER,
+                    id: salesOrderId
+                });
+
+                var customFieldChecked = salesOrder.getValue('custbody_jj_hold_fulfillment_otp_7886'); 
+
+                log.debug(customFieldChecked);
+                
+
+                if (customFieldChecked) {
+
+                    throw new Error('Bulk item fulfillment is restricted.');
+                }
+            
+        }
+            
+
+        }
+
+        /**
+         * Defines the function definition that is executed after record is submitted.
+         * @param {Object} scriptContext
+         * @param {Record} scriptContext.newRecord - New record
+         * @param {Record} scriptContext.oldRecord - Old record
+         * @param {string} scriptContext.type - Trigger type; use values from the context.UserEventType enum
+         * @since 2015.2
+         */
+        const afterSubmit = (scriptContext) => {
+        }
+
+        return {beforeLoad, beforeSubmit, afterSubmit}
+
+    });
